f5bc0735469f480582325c21776029eb1b82c5a9
CostaGit33 <192040298+CostaGit33@users.noreply.github.com>
2026-04-05 14:32:46 -0300

    Allow production host in Vite preview

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/",
+  preview: {
+    host: "0.0.0.0",
+    port: 80,
+    allowedHosts: ["barbeariadofoguinho.online", "www.barbeariadofoguinho.online"]
+  }
 });
